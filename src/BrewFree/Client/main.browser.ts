36157ab62530f5b9e54a3fbdda79faecd14dad09
John Powell <support@brewfree.org>
2017-05-20 11:50:26 -0400

    Added font-awesome

--- src/BrewFree/Client/main.browser.ts
+++ src/BrewFree/Client/main.browser.ts
@@ -2,7 +2,6 @@ import './polyfills/browser.polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAppModule } from './app/browser-app.module';
-
 const rootElemTagName = 'app'; // Update this if you change your root component selector
 
 // // Enable either Hot Module Reloading or production mode
